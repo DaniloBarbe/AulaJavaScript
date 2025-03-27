decd387c2e6717976ac4f3419022668bc0bea8f2
DaniloBarbe <danilobarbe4@gmail.com>
2025-03-27 11:07:41 -0300

    27/03

--- aula2/exercicio.js
+++ aula2/exercicio.js
@@ -1,6 +1,6 @@
 function e1 ()
 {
-    let n1 = document.getElementById("n1").value;
+    let n1 = Number(document.getElementById("n1").value);
 
     let quadrado = Number (n1) **2 ;
     let cubo = Number(n1) **3;
@@ -42,12 +42,42 @@ function e5()
 }
 function e6()
 {
-let qdt = document.getElementById("qdt").value;
-let preco = document.getElementById("preco").value;
-let valor = qdt*preco;
-let desconto = valor*0.1;
-let total= valor-desconto;
-document.getElementById("mensagem").innnerHTML="o preco total é: "+valor;
-document.getElementById("mensagem").innnerHTML="<br> o desconto é: "+desconto;
-document.getElementById("mensagem").innnerHTML="<br> o preço final é: "+total;
+    let qdt = document.getElementById("qdt").value;
+    let preco = document.getElementById("preco").value;
+    let valor = qdt*preco;
+    let desconto = valor*0.1;
+    let total= valor-desconto;
+    document.getElementById("mensagem").innerHTML="o preco total é: "+valor;
+    document.getElementById("mensagem").innerHTML="<br> o desconto é: "+desconto;
+    document.getElementById("mensagem").innerHTML="<br> o preço final é: "+total;
 }    
+function e7()
+{
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+    let n3 = Number(document.getElementById("n3").value);
+    let media = (n1+n2+n3)/3;
+    document.getElementById("mensagem").innerHTML="a media é: "+media;
+}
+function e8()
+{
+    let peso = Number(document.getElementById("n1").value);
+    let altura = Number(document.getElementById("n2").value);
+    let imc = peso/(altura*altura);
+    document.getElementById("mensagem").innerHTML="seu imc é: "+peso;
+}
+function e9()
+{
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+    let n3 = Number(document.getElementById("n3").value);
+    let media = n1*(1+((n2/100)*n3));
+    document.getElementById("mensagem").innerHTML="o montante é: "+media;
+}
+function e10()
+{
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+    let media = n2*n1*30;
+    document.getElementById("mensagem").innerHTML="o salario é: "+media;
+}
